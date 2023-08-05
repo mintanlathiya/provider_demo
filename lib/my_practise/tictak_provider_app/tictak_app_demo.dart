ea6c86edbbc8a64d350e949e850be69713d2f103
mintanlathiya <lathiyamintan1998@gmail.com>
2023-08-05 13:06:29 +0530

    commit

---
+++ lib/my_practise/tictak_provider_app/tictak_app_demo.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_demo/my_practise/tictak_provider_app/tictak_provider.dart';
+
+class TicTacAppDemoUi extends StatefulWidget {
+  const TicTacAppDemoUi({super.key});
+
+  @override
+  State<TicTacAppDemoUi> createState() => _TicTacAppDemoUiState();
+}
+
+class _TicTacAppDemoUiState extends State<TicTacAppDemoUi> {
+  @override
+  Widget build(BuildContext context) {
+    print('build');
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Padding(
+        padding: const EdgeInsets.all(15),
+        child: Column(
+          children: [
+            Consumer<TikTacmethodProvider>(
+              builder: (context, obj, child) => Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      const Text(
+                        'PLAYER 1',
+                        style: TextStyle(fontSize: 45, color: Colors.white),
+                      ),
+                      Text(
+                        obj.oScore.toString(),
+                        style:
+                            const TextStyle(fontSize: 45, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      const Text(
+                        'PLAYER 2',
+                        style: TextStyle(fontSize: 45, color: Colors.white),
+                      ),
+                      Text(
+                        obj.xScore.toString(),
+                        style:
+                            const TextStyle(fontSize: 45, color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Consumer<TikTacmethodProvider>(
+              builder: (context, obj, child) => Expanded(
+                child: GridView.builder(
+                  itemCount: 9,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                      mainAxisSpacing: 5,
+                      crossAxisSpacing: 5),
+                  itemBuilder: (context, index) => GestureDetector(
+                    onTap: () {
+                      context
+                          .read<TikTacmethodProvider>()
+                          .tapped(index, context);
+                    },
+                    child: Card(
+                      color: obj.cardColor[index],
+                      elevation: 2,
+                      shape: Border.all(
+                        color: Colors.white,
+                        width: 2,
+                      ),
+                      child: Center(
+                        child: Text(
+                          obj.displayElement[index],
+                          style: const TextStyle(
+                              fontSize: 80,
+                              fontStyle: FontStyle.italic,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Consumer<TikTacmethodProvider>(
+                  builder: (context, obj, child) => MaterialButton(
+                    onPressed: () {
+                      obj.clearScoreBoard();
+                    },
+                    shape: BeveledRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    height: 70,
+                    color: Colors.white,
+                    child: const Text(
+                      'Clear Score Board',
+                      style: TextStyle(
+                        fontSize: 40,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+                Consumer<TikTacmethodProvider>(
+                  builder: (context, obj, child) => MaterialButton(
+                    onPressed: () {
+                      obj.clearBoard();
+                    },
+                    shape: BeveledRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    height: 70,
+                    color: Colors.white,
+                    child: const Text(
+                      'Restart',
+                      style: TextStyle(
+                        fontSize: 40,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/my_practise/tictak_provider_app/tictak_provider.dart
@@ -0,0 +1,158 @@
+import 'package:flutter/material.dart';
+
+class TikTacmethodProvider extends ChangeNotifier {
+  bool oTurn = true;
+
+  List<String> displayElement = ['', '', '', '', '', '', '', '', ''];
+  List cardColor = List.generate(9, (index) => Colors.teal);
+
+  int oScore = 0;
+  int xScore = 0;
+  int filledBoxes = 0;
+
+  void tapped(int index, context) {
+    if (oTurn && displayElement[index] == '') {
+      displayElement[index] = 'O';
+      cardColor[index] = Colors.amber;
+
+      filledBoxes++;
+    } else if (!oTurn && displayElement[index] == '') {
+      displayElement[index] = 'X';
+      cardColor[index] = Colors.brown;
+      filledBoxes++;
+    }
+
+    oTurn = !oTurn;
+    checkWinner(context);
+    notifyListeners();
+  }
+
+  void checkWinner(context) {
+    // Checking rows
+    if (displayElement[0] == displayElement[1] &&
+        displayElement[0] == displayElement[2] &&
+        displayElement[0] != '') {
+      showWinDialog(displayElement[0], context);
+      clearBoard();
+    }
+    if (displayElement[3] == displayElement[4] &&
+        displayElement[3] == displayElement[5] &&
+        displayElement[3] != '') {
+      showWinDialog(displayElement[3], context);
+      clearBoard();
+    }
+    if (displayElement[6] == displayElement[7] &&
+        displayElement[6] == displayElement[8] &&
+        displayElement[6] != '') {
+      showWinDialog(displayElement[6], context);
+      clearBoard();
+    }
+
+    // Checking Column
+    if (displayElement[0] == displayElement[3] &&
+        displayElement[0] == displayElement[6] &&
+        displayElement[0] != '') {
+      showWinDialog(displayElement[0], context);
+      clearBoard();
+    }
+    if (displayElement[1] == displayElement[4] &&
+        displayElement[1] == displayElement[7] &&
+        displayElement[1] != '') {
+      showWinDialog(displayElement[1], context);
+      clearBoard();
+    }
+    if (displayElement[2] == displayElement[5] &&
+        displayElement[2] == displayElement[8] &&
+        displayElement[2] != '') {
+      showWinDialog(displayElement[2], context);
+      clearBoard();
+    }
+
+    // Checking Diagonal
+    if (displayElement[0] == displayElement[4] &&
+        displayElement[0] == displayElement[8] &&
+        displayElement[0] != '') {
+      showWinDialog(displayElement[0], context);
+      clearBoard();
+    }
+    if (displayElement[2] == displayElement[4] &&
+        displayElement[2] == displayElement[6] &&
+        displayElement[2] != '') {
+      showWinDialog(displayElement[2], context);
+      clearBoard();
+    } else if (filledBoxes == 9) {
+      showDrawDialog(context);
+      clearBoard();
+    }
+    notifyListeners();
+  }
+
+  void showWinDialog(String winner, context) {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("\" $winner \" is Winner!!!"),
+            actions: [
+              TextButton(
+                child: const Text("Play Again"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
+
+    if (winner == 'O') {
+      oScore++;
+    } else if (winner == 'X') {
+      xScore++;
+    }
+    notifyListeners();
+  }
+
+  void showDrawDialog(context) {
+    showDialog(
+        barrierDismissible: false,
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text("Draw"),
+            actions: [
+              TextButton(
+                child: const Text("Play Again"),
+                onPressed: () {
+                  clearBoard();
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        });
+    notifyListeners();
+  }
+
+  void clearBoard() {
+    for (int i = 0; i < 9; i++) {
+      displayElement[i] = '';
+      cardColor[i] = Colors.red;
+    }
+
+    filledBoxes = 0;
+    notifyListeners();
+  }
+
+  void clearScoreBoard() {
+    xScore = 0;
+    oScore = 0;
+    for (int i = 0; i < 9; i++) {
+      displayElement[i] = '';
+      cardColor[i] = Colors.red;
+    }
+
+    filledBoxes = 0;
+    notifyListeners();
+  }
+}
